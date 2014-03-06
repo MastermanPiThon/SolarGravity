04c1313c18a3f2f7ce96d03af366ccc861766076
simeonr <svr408@gmail.com>
2014-03-06 20:48:50 +0000

    work after day 2

--- day1.py
+++ day1.py
@@ -13,19 +13,38 @@ screen = pygame.display.set_mode((width, height))
 #define the class planet
 class planet:
     """ a planet """
-    def __init__(self, size, distance, color):
+    def __init__(self, size, distance, color, ipos):
         self.size = size
         self.distance = distance
         self.color = color
+        self.ipos = ipos
 
-    def positionx(distance, t)
+    def positionx(self, t):
         """ some mathy function """
+        R = self.distance
+        i = self.ipos
+        x = R * math.cos((t+i) / (R ** 1.5))
+        return x
 
-    def positiony(distance, t)
+    def positiony(self, t):
         """ some other mathy function """
-        
-    def drawme
+        R = self.distance
+        i = self.ipos
+        y = R * math.sin((t+i) / (R ** 1.5))
+        return y
 
+    def positionX(self,t):
+        
+    
+    def drawme(self, screen, t):
+        size = self.size
+        color = self.color
+        x = self.positionx(self, t) + 250
+        y = self.positiony(self, t) + 250
+        pygame.draw.circle(screen, color, (x,y), size, 0)
+            
+#class player:
+    
 
 t=0
 tick0 = pygame.time.get_ticks()
