e3eb64e3c2ba6cc4c4336350743935bb46381dd5
simeonr <svr408@gmail.com>
2014-03-20 19:46:54 +0000

    working on relative position functions and player position

--- day1.py
+++ day1.py
@@ -13,11 +13,12 @@ screen = pygame.display.set_mode((width, height))
 #define the class planet
 class planet:
     """ a planet """
-    def __init__(self, size, distance, color, ipos):
+    def __init__(self, size, distance, color, ipos, mass):
         self.size = size
         self.distance = distance
         self.color = color
         self.ipos = ipos
+        self.mass = mass
 
     def positionx(self, t):
         """ Absolute Abscissa Position """
@@ -33,8 +34,12 @@ class planet:
         y = R * math.sin((t+i) / (R ** 1.5))
         return y
 
-    #def positionX(self,t):
-        
+    def positionX(self, t, player, objects, timeinterval, scale):
+        ''' Relative Abscissa Position '''
+        xme = self.positionx(t)
+        xit = player.position(objects, t, timeinterval)
+        X = (xme - xit)/scale + 320
+        return X
         
     #def positionY(self,t):
         
@@ -48,9 +53,10 @@ class planet:
 
 #define the star class
 class star:
-    def __init__(self, size, color):
+    def __init__(self, size, color, mass):
         self.size = size
         self.color = color
+        self.mass = mass
 
     def positionx(self):
         """ Absolute Abscissa Position """
@@ -69,26 +75,38 @@ class star:
 
             
 class player:
-    fuel = 0
-    def __init__(self, size, color, x0, y0):
+    def __init__(self, size, color, x, y, vx, vy):
         self.size = size
         self.color = color
-        self.x0 = x0
-        self.y0 = y0
+        self.xarray = [x,x]
+        self.yarray = [y,y]
+        self.varray = [vx,vy]
+        self.aarray = [0,0]
+        
 
-    def positionx(self, t):
-        """ Absolute Abscissa Position """
-        R = self.distance
-        i = self.ipos
-        x = R * math.cos((t+i) / (R ** 1.5))
-        return x
+    def position(self, objects, t, timeinterval):
+        """ Absolute Position """
+        xarray[0] = xarray[1]
+        yarray[0] = yarray[1]
+        
+        for planet in objects:
+            xdistance = planet.positionx(t) - xarray[0]
+            ydistance = planet.positiony(t) - yarray[0]
+            r = (xdistance ** 2 + ydistance ** 2) ** 0.5
+            aarray[0] += planet.mass / r ** 3 * xdistance
+            aarray[1] += planet.mass / r ** 3 * ydistance
 
-    def positiony(self, t):
-        """ Absolute Ordinate Position """
-        R = self.distance
-        i = self.ipos
-        y = R * math.sin((t+i) / (R ** 1.5))
-        return y
+        #player input here
+
+        varray[0] += aarray[0]*timeinterval
+        varray[1] += aarray[1]*timeinterval
+
+        xarray[1] = varray[0]*timeinterval
+        yarray[1] = varray[1]*timeinterval
+
+        aarray = [0,0]
+        
+        return [array[1], yarray[1]]
 
     #def positionX(self,t):
         
@@ -118,6 +136,7 @@ while 1:
     sun.draw(screen)
     mercury.draw(screen, t)
     venus.draw(screen, t)
+    objects = [sun, mercury, venus]
 
     # Time Handling
     tick1 = pygame.time.get_ticks()
