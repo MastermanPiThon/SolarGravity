04809d7b7cc48b26446d78ab6bb77fbde794b208
simeonr <svr408@gmail.com>
2014-04-03 14:53:57 -0400

    Update StarClass.py

--- StarClass.py
+++ StarClass.py
@@ -1,1 +1,28 @@
-rtyrtyh
+#Import Library
+import math
+import random
+import pygame
+
+#import pygame.locals import * (what is this??)
+
+
+class star:
+    def __init__(self, size, color, mass):
+        self.size = size
+        self.color = color
+        self.mass = mass
+
+    def positionx(self):
+        """ Absolute Abscissa Position """
+        return 0
+
+    def positiony(self):
+        """ Absolute Ordinate Position """
+        return 0
+
+    def draw(self, screen):
+        size = self.size
+        color = self.color
+        x = int(self.positionx() + 320)
+        y = int(self.positiony() + 240)
+        pygame.draw.circle(screen, color, (x,y), size, 0)
