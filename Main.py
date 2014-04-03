96abb723d2d3913a0f4e397c353bfbcf884893f2
simeonr <svr408@gmail.com>
2014-04-03 14:57:11 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,6 +2,9 @@
 import math
 import random
 import pygame
+from PlanetClass.py import *
+from PlayerClass.py import *
+from StarClass.py import *
 
 #import pygame.locals import * (what is this??)
 
@@ -9,6 +12,56 @@ import pygame
 pygame.init()
 width, height = 640, 480
 screen = pygame.display.set_mode((width, height))
+ 
+ 
+t=0
+tick0 = pygame.time.get_ticks()
+tick1 = pygame.time.get_ticks()
+while 1:
+    screen.fill(0)
+    sun = star(15, 0xeee8aa)
+    mercury = planet(5, 50, 0x3366FF, 0)
+    venus = planet(10, 100, 0x4B0082, 0)
+    #x = int(math.floor(50*math.cos(t)))
+    #y = int(math.floor(30*math.sin(t)))
+    #pygame.draw.circle(screen, 0x3366FF, (250+ x,250 + y), 10, 0)
+    #pygame.draw.circle(screen, 0x3366FF, (250+ 2*x,250 + 2*y), 10, 0)
+    sun.draw(screen)
+    mercury.draw(screen, t)
+    venus.draw(screen, t)
+    objects = [sun, mercury, venus]
+
+    # Time Handling
+    tick1 = pygame.time.get_ticks()
+    t+=(tick1-tick0)*1.0
+    tick0 = tick1
+
+    # The Flip
+    pygame.display.flip()
+
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            pygame.quit()
+            exit(0)
+            
+            
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+            
+            
+            
 
 #define the class planet
 class planet:
@@ -158,33 +211,3 @@ class player:
         x = int(self.positionx(t) + 320)
         y = int(self.positiony(t) + 240)
         pygame.draw.circle(screen, color, (x,y), size, 0)
-
-t=0
-tick0 = pygame.time.get_ticks()
-tick1 = pygame.time.get_ticks()
-while 1:
-    screen.fill(0)
-    sun = star(15, 0xeee8aa)
-    mercury = planet(5, 50, 0x3366FF, 0)
-    venus = planet(10, 100, 0x4B0082, 0)
-    #x = int(math.floor(50*math.cos(t)))
-    #y = int(math.floor(30*math.sin(t)))
-    #pygame.draw.circle(screen, 0x3366FF, (250+ x,250 + y), 10, 0)
-    #pygame.draw.circle(screen, 0x3366FF, (250+ 2*x,250 + 2*y), 10, 0)
-    sun.draw(screen)
-    mercury.draw(screen, t)
-    venus.draw(screen, t)
-    objects = [sun, mercury, venus]
-
-    # Time Handling
-    tick1 = pygame.time.get_ticks()
-    t+=(tick1-tick0)*1.0
-    tick0 = tick1
-
-    # The Flip
-    pygame.display.flip()
-
-    for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            pygame.quit()
-            exit(0)
